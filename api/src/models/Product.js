1ab64ed551c47e2460f34ee12d8c9c388210ca39
Alberto Popelka <albertopopelka@gmail.com>
2020-08-12 09:09:18 -0300

    product data model

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -2,11 +2,18 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('product', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('product', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        price: {
+
+        }
+    });
 };
