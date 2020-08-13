aafe6864342503327cf3fcc479371aa78d892bb1
Julian Lucero <julianlucero2012@gmail.com>
2020-08-13 16:19:14 -0300

    Estructura de crud creada

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./containers/navbar/Navbar.jsx";
-import AddProductForm from "./components/add_product_form/AddProductForm.jsx";
+//Imports de prueba aqui antes del commit eliminarlos
+
+//
+
+//Solo dejar el navbar dentro de App, agregar y luego elimar para pruebas locales
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <AddProductForm />
     </div>
   );
 }
