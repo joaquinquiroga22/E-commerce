3cedfe0c4b285603f80ba1137c0e1e85aa79bf09
GerMoren <ingenieriamg91@gmail.com>
2020-08-13 09:47:35 -0300

    cambios menores en product.js

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,19 +1,19 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
-const { Category } = require('../db.js');
+const server = require("express").Router();
+const { Product } = require("../db.js");
+const { Category } = require("../db.js");
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
+server.get("/", (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.send(products);
+    })
+    .catch(next);
 });
-server.get('/Category/name:NameCategory', (req, res, next) => {   
-	Category.findAll()
-	    .then(Category =>{              
-			res.send(Category);          	
-		})          	
-		.catch(next);
-	});
+server.get("/category/:nameCategory", (req, res, next) => {
+  Category.findAll()
+    .then((category) => {
+      res.send(category);
+    })
+    .catch(next);
+});
 module.exports = server;
