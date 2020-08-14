48396aff3e11d988f80b25d2164fad6df857f606
Nicolas <cailletn@northlands.edu.ar>
2020-08-14 11:02:31 -0300

    Tasks t11, t9 y t15

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React , { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./containers/navbar/Navbar.jsx";
-import AddProductForm from "./components/add_product_form/AddProductForm.jsx";
+import ProductCard from './components/product_card/ProductCard.jsx'
+import Product from './container/view_product/Product.jsx'
+import {Route} from 'react-router-dom';
+import FormCategory from './components/categoryform/CrudAddProduct.jsx'
+
 function App() {
+  const [products, setProduct] = useState([]);
+  
+  function OnFilter(productId){
+    let product = products.filter(p => p.id == parseInt(productId))
+    if(product > 0){
+      return product[0] 
+    }
+
+    else{
+      alert("No hay productos con ese nombre")
+    }
+  }
+  
   return (
     <div className="App">
-      <Navbar />
-      <AddProductForm />
+      
+      <Navbar /> 
+      <Route path='/product/:productId' render={({match}) => <Product product={OnFilter(match.params.productId)}/>} />
+      <ProductCard /> 
+      <FormCategory />
+      {/* un get a la ruta del backedn que edvuelva ese id o que devuelva el producto que quieras / hasta que no esten las rutas no se puede seguir. / */ } 
     </div>
   );
 }
 
 export default App;
+
+// ProductCard ==> Product 
+// 
