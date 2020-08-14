d67d0c53321e14d1a7613c9fc8cd2f55f876c7a9
Julian Lucero <julianlucero2012@gmail.com>
2020-08-14 12:25:42 -0300

    Algunos archivos mas

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,40 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./containers/navbar/Navbar.jsx";
-import ProductCard from './components/product_card/ProductCard.jsx'
-import Product from './container/view_product/Product.jsx'
-import {Route} from 'react-router-dom';
-import FormCategory from './components/categoryform/CrudAddProduct.jsx'
+import ProductCard from "./components/product_card/ProductCard.jsx";
+import Product from "./container/view_product/Product.jsx";
+import { Route } from "react-router-dom";
 
 function App() {
   const [products, setProduct] = useState([]);
-  
-  function OnFilter(productId){
-    let product = products.filter(p => p.id == parseInt(productId))
-    if(product > 0){
-      return product[0] 
-    }
 
-    else{
-      alert("No hay productos con ese nombre")
+  function OnFilter(productId) {
+    let product = products.filter((p) => p.id == parseInt(productId));
+    if (product > 0) {
+      return product[0];
+    } else {
+      alert("No hay productos con ese nombre");
     }
   }
-  
+
   return (
     <div className="App">
-      
-      <Navbar /> 
-      <Route path='/product/:productId' render={({match}) => <Product product={OnFilter(match.params.productId)}/>} />
-      <ProductCard /> 
-      <FormCategory />
-      {/* un get a la ruta del backedn que edvuelva ese id o que devuelva el producto que quieras / hasta que no esten las rutas no se puede seguir. / */ } 
+      <Navbar />
+      <Route
+        path="/product/:productId"
+        render={({ match }) => (
+          <Product product={OnFilter(match.params.productId)} />
+        )}
+      />
+      <ProductCard />
+      {/* un get a la ruta del backedn que edvuelva ese id o que devuelva el producto que quieras / hasta que no esten las rutas no se puede seguir. / */}
     </div>
   );
 }
 
 export default App;
 
-// ProductCard ==> Product 
-// 
+// ProductCard ==> Product
+//
+
