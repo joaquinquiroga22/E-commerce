e5b8f28f2d88d3fc04b5030745d7ed54e5947b2f
GerMoren <ingenieriamg91@gmail.com>
2020-08-14 12:49:59 -0300

    completo t18 t19 t20 y t22

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,33 +1,34 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const router = Router();
-const categoryRouter = require('./category.js')
-const productRouter = require('./product.js')
-const {Product} = require("../db.js");
-router.use('/products/category', categoryRouter)
-const { Sequelize } = require('sequelize');
+const categoryRouter = require("./category.js");
+const productRouter = require("./product.js");
+const { Product } = require("../db.js");
+const { Sequelize } = require("sequelize");
+router.use("/products/category", categoryRouter);
 
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
-router.use('/products', productRouter);
-router.get('/search', (req,res,next) =>{
-    Product.findAll({
-        where: { 
-            //$or:[
-            //{description: {
-                //[Sequelize.Op.any]: [req.query.query]
-            //}
-        },
-            //{
-                name: {
-                [Sequelize.Op.any]: [req.query.query]
-           // }
-        }
-        //]
+router.use("/products", productRouter);
+router.get("/search", (req, res, next) => {
+  Product.findAll({
+    where: {
+      //$or:[
+      //{description: {
+      //[Sequelize.Op.any]: [req.query.query]
+      //}
+    },
+    //{
+    name: {
+      [Sequelize.Op.any]: [req.query.query],
+      // }
+    },
+    //]
     //}
-    }).then((products) =>{
-        console.log(products[0]);
-        res.send(products[0]);
+  })
+    .then((products) => {
+      console.log(products[0]);
+      res.send(products[0]);
     })
     .catch(next);
 });
--- api/tests/routes/product.spec.js
+++ api/tests/routes/product.spec.js
@@ -1,24 +1,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Product, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Product, conn } = require("../../src/db.js");
 
 const agent = session(app);
 const product = {
-  name: 'producto',
+  name: "producto",
+  price: 300,
+  stock: 25,
+  description: "description",
 };
 
-describe('PRODUCT routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Product.sync({ force: true })
-    .then(() => Product.create(product)));
-  describe('GET /products', () => {
-    it('should get 200', () => 
-      agent.get('/products/').expect(200)
-    );
+describe("PRODUCT routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  beforeEach(() =>
+    Product.sync({ force: true }).then(() => Product.create(product))
+  );
+  describe("GET /products", () => {
+    it("should get 200", () => agent.get("/products/").expect(200));
   });
 });
