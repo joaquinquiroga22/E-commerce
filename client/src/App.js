1fd4a4527ec575c3b9de57a04679467dced6e0d6
Julian Lucero <julianlucero2012@gmail.com>
2020-08-14 11:56:16 -0300

    Componente crudViewProduct y delete product finalizado

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import "./App.css";
 import Navbar from "./containers/navbar/Navbar.jsx";
 //Imports de prueba aqui antes del commit eliminarlos
 import Crud from "./containers/crud/Crud.jsx";
-import CrudUpdateProduct from "./components/crud_update_product/CrudUpdateProduct.jsx"; //
+import CrudViewProduct from "./components/crud_view_product/CrudViewProduct.jsx"; //
 
 //Solo dejar el navbar dentro de App, agregar y luego elimar para pruebas locales
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <CrudUpdateProduct />
+      <CrudViewProduct />
     </div>
   );
 }
--- client/src/components/crud_view_product/CrudViewProduct.jsx
+++ client/src/components/crud_view_product/CrudViewProduct.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import testImage from "../../img/default.jpg";
+import s from "./CrudViewProduct.module.css";
 export default function CrudDeleteProduct() {
   const props = {
     name: "Aloe vera",
-    description: "Plata con poderosos poderes curativos",
+    description:
+      "Plata con poderosos poderes curativos, todo este texto es solor para rellenar porque no tengo el lorem insupm jeje igual antes era chevere ajaj ajejkajfae ea eafjeaf eakfajnk sefseklfsef skelfsekflsefs klnfeslknflse seklsneflkneslkfnsel fselkflesknfls fklneslkfnselkfnes fesnfkesnlfsn fleksnflesnlfes feknslknfeskl fesknflseknfkles fleksnflkesnlkfse lfneslkfneslk lknfeslknfeslk klfeslknfels nfkesnflesn klfneslkfnselk",
     price: 403,
     image: testImage,
     stock: 32,
   };
   return (
-    <div>
-      <h1>
-        <img src={props.image} />
-        Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      </h1>
-      <br /> <p>hoplaaaaaa</p>
-    </div>
+    <form className={s.form}>
+      <div className={s.viewProduct}>
+        <div className={s.image}>
+          <img src={testImage} />
+        </div>
+        <div className={s.info}>
+          <h2>{props.name}</h2>
+          <div>
+            <h4>Descripcion</h4>
+            <p>{props.description}</p>
+          </div>
+          <div>
+            <h4>Precio</h4>
+            <p>{props.price}</p>
+          </div>
+          <div>
+            <h4>Stock</h4>
+            <p>{props.stock}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <input className={s.deleteBtn} type="submit" value="Eliminar" />
+        <input type="button" value="Cancelar" />
+      </div>
+    </form>
   );
 }
