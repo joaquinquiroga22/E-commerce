e7841fda69c09516cd2dcf240e9eb80adb27b57b
GerMoren <ingenieriamg91@gmail.com>
2020-08-15 11:30:36 -0300

    terminado buscador

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,35 +2,15 @@ const { Router } = require("express");
 const router = Router();
 const categoryRouter = require("./category.js");
 const productRouter = require("./product.js");
+const searchRouter = require("./search.js");
 const { Product } = require("../db.js");
 const { Sequelize } = require("sequelize");
-router.use("/products/category", categoryRouter);
 
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
+router.use("/products/category", categoryRouter);
 router.use("/products", productRouter);
-router.get("/search", (req, res, next) => {
-  Product.findAll({
-    where: {
-      //$or:[
-      //{description: {
-      //[Sequelize.Op.any]: [req.query.query]
-      //}
-    },
-    //{
-    name: {
-      [Sequelize.Op.any]: [req.query.query],
-      // }
-    },
-    //]
-    //}
-  })
-    .then((products) => {
-      console.log(products[0]);
-      res.send(products[0]);
-    })
-    .catch(next);
-});
+router.use("/search", searchRouter);
 
 module.exports = router;
