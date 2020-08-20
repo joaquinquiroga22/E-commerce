d04ba5d8f1ce977d0d38eeeef42a9e7f91419986
Julian Lucero <julianlucero2012@gmail.com>
2020-08-20 14:05:39 -0300

    Start t43

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //Componentes
@@ -7,31 +7,39 @@ import Product from "./components/view_product/Product.jsx";
 import Catalogue from "./containers/catalogue/Catalogue.jsx";
 import Crud from "./containers/crud/Crud.jsx";
 
-//React-Redux
-import { Provider } from "react-redux";
-import store from "./store/main.js";
+
+
+import { useSelector, useDispatch } from "react-redux";
+//importamos la accion a dispatchear
+import { getProducts } from "./actions/products.js";
 
 function App() {
   const [search, setSearch] = useState([]);
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [getProducts]);
 
   const searchQuery = function (valor) {
     setSearch(valor);
   };
   return (
-    < Provider store={store} >
-      <Router>
-        <div className="App">
-          <Route path="/" render={() => <Navbar onSearch={searchQuery} />} />
-          <Route exact path="/admin" component={Crud} />
-          <Route exact path="/catalogo" component={Catalogue} />
-          <Route
-            exact
-            path="/product/:id"
-            render={({ match }) => <Product id={match.params.id} />}
-          />
-        </div>
-      </Router>
-    </Provider >
+
+    <Router>
+      <div className="App">
+        <Route path="/" render={() => <Navbar onSearch={searchQuery} />} />
+        <Route exact path="/admin" component={Crud} />
+        <Route exact path="/catalogo" component={Catalogue} />
+        <Route
+          exact
+          path="/product/:id"
+          render={({ match }) => <Product id={match.params.id} />}
+        />
+      </div>
+    </Router>
+
   );
 }
 
