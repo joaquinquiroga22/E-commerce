d93e562f89eb2fe7f2da3d04c2f352f5a315dd79
Nicolas <cailletn@northlands.edu.ar>
2020-08-21 13:59:11 -0300

    Validaciones de rutas

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -14,45 +14,82 @@ server.post("/", (req, res, next) => {
     .catch((error) => {
       next(error);
     });
-});
+}); 
 
 server.get("/", (req, res, next) => {
   const key = Object.keys(req.query);
   const status = req.query[key];
-  Order.findAll({ where: { state: status }, include: User })
-    .then((orders) => {
-      res.send(orders);
-    })
-    .catch((error) => next(error));
+  if(status){
+    Order.findAll({ where: { state: status }, include: User })
+      .then((orders) => {
+        if(orders && orders.length === 0) {
+          return res.status(400).json({message: `No hay ordenes con status: ${status}`})
+        }
+        res.send(orders);
+      })
+      .catch((error) => next(error));
+  } else {
+
+    Order.findAll({ include: User })
+      .then((orders) => {
+        if(orders && orders.length === 0) {
+          return res.status(400).json({message: `No se ha creado ninguna orden`})
+        }
+        res.send(orders);
+      })
+      .catch((error) => next(error));
+  }
+  
 });
 
 server.get("/:id", (req, res, next) => {
+  const idOrder = req.params.id
   Order.findAll({
-    where: { id: req.params.id },
+    where: { id: idOrder },
     include: User,
   })
     .then((orders) => {
       res.send(orders);
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      next(error);
+    })
 });
 
 server.put("/:id", (req, res, next) => {
-  const { status, address } = req.body;
-  if (status || address) {
+  const { state, address } = req.body;
+  if (state && address) {
     Order.update(
-      { status, address },
+      { state, address },
       { where: { id: req.params.id }, returning: true }
     )
       .then((orders) => {
-        console.log(orders);
         res.send(orders[1][0].dataValues);
       })
       .catch((error) => next(error));
-  } else {
+  } else if(state) {
+    Order.update(
+      { state },
+      { where: { id: req.params.id }, returning: true }
+    )
+      .then((orders) => {
+        res.send(orders[1][0].dataValues);
+      })
+      .catch((error) => next(error));
+  }
+  else if(address) {
+    Order.update(
+      { address },
+      { where: { id: req.params.id }, returning: true }
+    )
+      .then((orders) => {
+        res.send(orders[1][0].dataValues);
+      })
+      .catch((error) => next(error));
+  } else { 
     res
       .status(400)
-      .json({ message: "Debe pasar un status o address para modificar" });
+      .json({ message: "Debe pasar un state o address para modificar" });
   }
 });
 
