96decd94c71e15b0e8ec88233ac6f4f2d6a2e7d5
Nicolas <cailletn@northlands.edu.ar>
2020-08-25 12:02:28 -0300

    Nuevo icono

--- client/src/containers/admin_page/AdminPage.jsx
+++ client/src/containers/admin_page/AdminPage.jsx
@@ -3,6 +3,7 @@ import s from './AdminPage.module.css';
 import Crud from '../crud/Crud.jsx';
 import OrdersTable from '../orders_table/OrdersTable.jsx';
 import Categories from '../category_page/CategoryAdmin.jsx'
+import PermMediaIcon from '@material-ui/icons/PermMedia';
 
 //material ui
 import ReceiptIcon from '@material-ui/icons/Receipt';
@@ -43,7 +44,7 @@ export default function AdminPage() {
           <input type="radio" onChange={(e) => renderComponent(e)} id="manage_products" name="menu" value="products"/>
           <label htmlFor="manage_products"><StoreIcon className={s.icon} />Gestion de productos</label>
           <input type="radio" onChange={(e) => renderComponent(e)} id="manage_categories" name="menu" value="categories"/>
-          <label htmlFor="manage_categories"><StoreIcon className={s.icon} />Gestion de categorias</label>
+          <label htmlFor="manage_categories"><PermMediaIcon className={s.icon} />Gestion de categorias</label>
         </div>
         <div className={s.main}>
           {component && component}
