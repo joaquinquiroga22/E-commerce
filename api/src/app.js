0c6f0f3859d67440ff15b1c19b8951d0e00c0eca
Julian Lucero <julianlucero2012@gmail.com>
2020-08-27 10:07:37 -0300

    passport prueba 1, accion

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,61 @@
 const express = require("express");
+const session = require('express-session');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const cors = require("cors");
-// var passport = require('passport');
-// var Strategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 require("./db.js");
+
+//Modelo de usuario
+const { User } = require("./db.js");
+
+
+passport.use(new Strategy({ usernameField: 'email', passwordField: 'password'},
+ function(username, password, done) {
+    User.findOne({where: { email: username }}).then((user) => {
+        if(!user) {
+          return done(null, false);
+        }
+        bcrypt.compare(password, user.password).then((res) => {
+            // res === true
+            console.log(res)
+            if(res){
+              return done(null, user);
+            }
+            return done(null, false);
+        });
+      })
+    .catch(err => {
+      return done(err);
+    })
+}));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findOne({where: { id: id}})
+    .then((user) => {
+      done(null, user);
+    })
+    .catch(err => {
+      return done(err);
+    })
+});
 
 const server = express();
 //Middlewares
 //Usamos el modulo cors para las politics cors
 
+
 server.name = "API";
-server.use(cors());
+server.use(cors({
+  origin: "http://localhost:3001",
+  credentials: true}));
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
@@ -21,20 +63,52 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+server.use(session({ secret: "clasificado", resave: false, saveUninitialized: false }));
 
-// app.use(require('express-session')({
-//   secret: 'secret',
-//   resave: false,
-//   saveUninitialized: false
-// }));
 
+server.use(passport.initialize());
+server.use(passport.session());
+
+
+
+server.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+
+server.post('/login',  passport.authenticate('local'),
+  function(req, res) {
+    res.send("te logeaste")
+  });
+
+function isAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    console.log("logeado")
+    next();
+  } else {
+    console.log("no logeado")
+    res.send('no logeado');
+  }
+}
+
+server.get('/profile',
+  isAuthenticated,
+  function(req, res){
+     console.log(req.sessionID)
+    res.send("Estas en tu perfil");
+});
+
+server.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
 
 server.use("/", routes);
 
@@ -46,42 +120,6 @@ server.use((err, req, res, next) => {
   console.error(err);
   res.status(status).send(message);
 });
-
-// passport.use(new Strategy(
-//   function(username, password, done) {
-//     db.users.findByUsername(username)
-//       .then((user) => {
-//         if(!user) {
-//           return done(null, false);
-//         }
-//         if(user.password != password) {
-//           return done(null, false);
-//         }
-//         return done(null, user);
-//       })
-//     .catch(err => {
-//       return done(err);
-//     })
-//   }));
-
-//   passport.serializeUser(function(user, done) {
-//     done(null, user.id);
-//   });
-  
-  // Al deserealizar la información del usuario va a quedar almacenada en req.user
-  
-  // passport.deserializeUser(function(id, done) {
-  //   db.users.findByPk(id)
-  //     .then((user) => {
-  //       done(null, user);
-  //     })
-  //     .catch(err => {
-  //       return done(err);
-  //     })
-  // });
-  
-
-
 
 
 module.exports = server;
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,17 +1,31 @@
 const server = require("express").Router();
+const bcrypt = require('bcryptjs');
 const { User, Order, Productsorder, Product } = require("../db.js");
 
 server.post("/", (req, res, next) => {
   let { email, name, lastname, password, role } = req.body;
   if (email && name && lastname && password && role) {
-    User.create({ email, name, lastname, password, role })
-      .then((users) => {
-        res.status(201);
-        res.send(users.dataValues);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            // Store hash in your password DB.
+            const newUser = {
+              email: email,
+              name: name,
+              lastname: lastname,
+              password: hash,
+              role: role
+            }
+            User.create(newUser)
+              .then((users) => {
+                res.status(201);
+                res.send(users.dataValues);
+              })
+              .catch((error) => {
+                res.send(error);
+              });
+        });
+    });
+
 
   } else {
     return res.status(400).json({ message: "Debe pasar los parametros requeridos." });
