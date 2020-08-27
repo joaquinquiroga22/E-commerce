6bb99b681a26847cb7bda5e2a0621b49d655b49e
Alberto M. Popelka <63745642+apopelka87@users.noreply.github.com>
2020-08-27 15:04:50 -0300

    product card showing reviews

--- client/src/components/view_product/Product.jsx
+++ client/src/components/view_product/Product.jsx
@@ -1,15 +1,18 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+import { Container, Box, List } from "@material-ui/core";
+
 import s from "./Product.module.css";
 import Rating from "@material-ui/lab/Rating";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+
 import Button from "@material-ui/core/Button";
 
 //Importamos de redux para poder conectar al estado y poder dispatchear actions
 import { useSelector, useDispatch } from "react-redux";
 //importamos la funcion a dispatchear
 import { getProduct } from "../../actions/products.js";
-
+import ReviewCard from "../reviews/ReviewCard";
 
 export default function Product({ id }) {
   const [value, setValue] = useState(2);
@@ -27,45 +30,58 @@ export default function Product({ id }) {
   }, [getProduct]);
   console.log(product);
   return (
-    <div className={s.container}>
-      <div className={s.img} >
-        <img className={s.imagen} alt={product.name} src={product.image} />
-      </div>
-
-      <div className={s.body}>
-        <div className={s.izquierda}>
+    <Container
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      m={5}
+    >
+      <Box display="flex" justifyContent="center">
+        <Box className={s.img}>
+          <img className={s.imagen} alt={product.name} src={product.image} />
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-around"
+        >
           <h2 className={s.title}> {product.name} </h2>
           <p className={s.e}> {product.description} </p>
           <h5> Quedan: {product.stock}</h5>
           <h3 className={s.num}> ${product.price} </h3>
-          <div className={s.rating}>
-            <Box
-              display="flex"
-              flexDirection="row"
-              component="fieldset"
-              mb={3}
-              borderColor="transparent"
-            >
-              <Typography component="legend"> Opinion: </Typography>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </Box>
-          </div>
-        </div>
+          <Box>
+            <Button variant="contained" color="secondary">
+              Comprar ya
+            </Button>
+            <Button variant="contained" color="primary">
+              Carrito
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      <Box m={5} display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h5"> Reviews </Typography>
 
-        <div className={s.buttoms}>
-          <Button variant="contained"> Comprar ya</Button>
-          <Button variant="contained" color="primary">
+        {product.reviews && product.reviews.length > 0 ? (
+          product.reviews.map((review) => {
+            return (
+              <ReviewCard
+                key={review.id}
+                title={review.title}
+                stars={review.stars}
+                description={review.description}
+                user={review.image}
+                date={review.updatedAt}
+              />
+            );
+          })
+        ) : (
+          <Typography variant="h6" color="error">
             {" "}
-            Carrito{" "}
-          </Button>
-        </div>
-      </div>
-    </div>
+            Este Producto no posee reviews Aun{" "}
+          </Typography>
+        )}
+      </Box>
+    </Container>
   );
 }
